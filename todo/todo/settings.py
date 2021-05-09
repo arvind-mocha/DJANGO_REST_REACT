91d581c596d28aa71a0818e19c6b7b879f9f2a2a
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-05-09 14:54:50 +0530

    Initial commit

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Only these urls can get data from our api
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
